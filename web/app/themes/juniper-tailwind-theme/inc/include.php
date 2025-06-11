5d09f8411da676e2404dbaf7ffb56f16fe7f8309
Hernan <hernandariobs@gmail.com>
2025-06-11 15:34:29 -0500

    feat: quite referencias inexistentes en include.php

--- web/app/themes/juniper-tailwind-theme/inc/include.php
+++ web/app/themes/juniper-tailwind-theme/inc/include.php
@@ -5,5 +5,4 @@ require_once 'pattern-categories.php';
 
 $juniper_blocks = new \Juniper\Blocks\JuniperBlocks();
 $juniper_blocks->include_blocks_functions();
-$juniper_cpt_example       = new \Juniper\Cpt\Cpt_example();
-$juniper_categoria_ejemplo = new \Juniper\Taxonomies\categoria_ejemplo();
+
